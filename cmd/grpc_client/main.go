85737b0e2e991853f3dbb47259d9a60a65beb230
Shamistan Alyzade <stalyzade@avito.ru>
2024-03-15 23:25:44 +0400

    week_2 fix build

--- cmd/grpc_client/main.go
+++ cmd/grpc_client/main.go
@@ -1,45 +1,57 @@
 package main
 
-//const (
-//	serviceAddress = "localhost:50052"
-//	userID         = 1
-//)
+import (
+	"context"
+	"log"
+	"time"
 
-//func main() {
-//	conn, err := grpc.Dial(serviceAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
-//	if err != nil {
-//		log.Fatalln("failed to connect to service:", err.Error())
-//	}
-//	defer conn.Close() //nolint:errcheck
-//
-//	c := pb.NewChatV1Client(conn)
-//
-//	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-//	defer cancel()
-//
-//	runClientMethods(ctx, c)
-//}
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
-//func runClientMethods(ctx context.Context, c pb.ChatV1Client) {
-//	_, err := c.Delete(ctx, &pb.DeleteRequest{Id: userID})
-//	if err != nil {
-//		log.Println("failed to delete user: ", err.Error())
-//	}
-//
-//	resCreate, err := c.Create(ctx, &pb.CreateRequest{
-//		UserNames: []string{"name_1", "name2"},
-//	})
-//	if err != nil {
-//		log.Println("failed to Create user: ", err.Error())
-//	}
-//	log.Printf("create response: %+v\n", resCreate)
-//
-//	_, err = c.SendMessage(ctx, &pb.SendMessageRequest{
-//		From:      "test_from",
-//		Text:      "test_text",
-//		Timestamp: &timestamp.Timestamp{},
-//	})
-//	if err != nil {
-//		log.Println("failed to SendMessage: ", err.Error())
-//	}
-//}
+	pb "github.com/Shemistan/chat_server/pkg/chat_api_v1"
+)
+
+const (
+	serviceAddress = "localhost:50052"
+	userID         = 1
+)
+
+func main() {
+	conn, err := grpc.Dial(serviceAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln("failed to connect to service:", err.Error())
+	}
+	defer conn.Close() //nolint:errcheck
+
+	c := pb.NewChatV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	runClientMethods(ctx, c)
+}
+
+func runClientMethods(ctx context.Context, c pb.ChatV1Client) {
+	_, err := c.DeleteChat(ctx, &pb.DeleteChatRequest{ChatId: userID})
+	if err != nil {
+		log.Println("failed to delete user: ", err.Error())
+	}
+
+	resCreate, err := c.Create(ctx, &pb.CreateRequest{
+		ChatName:   "test",
+		UserLogins: []string{"test_1", "test_2"},
+	})
+	if err != nil {
+		log.Println("failed to Create user: ", err.Error())
+	}
+	log.Printf("create response: %+v\n", resCreate)
+
+	_, err = c.SendMessage(ctx, &pb.SendMessageRequest{
+		ChatName:  "test",
+		Message:   "test",
+		UserLogin: "test_1",
+	})
+	if err != nil {
+		log.Println("failed to SendMessage: ", err.Error())
+	}
+}
